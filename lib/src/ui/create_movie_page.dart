3b090faae759d1e9cc240b0d8bf401827dc0ba63
lathifrdp <lathifrdp@gmail.com>
2022-11-03 21:54:58 +0700

    init create dan detail movie page

---
+++ lib/src/ui/create_movie_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CreateMoviePage extends StatefulWidget {
+  const CreateMoviePage({Key? key}) : super(key: key);
+
+  @override
+  State<CreateMoviePage> createState() => _CreateMoviePageState();
+}
+
+class _CreateMoviePageState extends State<CreateMoviePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
